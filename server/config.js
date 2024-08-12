5fdc7b4f21f677385f54ab96a51485a43024436c
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2024-08-12 20:26:09 +0530

    add mongodb env var & config

--- server/config.js
+++ server/config.js
@@ -5,6 +5,8 @@ dotenv.config();
 const NODE_ENV = process.env?.NODE_ENV?.toLocaleLowerCase();
 const PORT = process.env?.PORT || 3000;
 const CORS_ORIGIN = process.env?.CORS_ORIGIN;
+const MONGODB_URI = process.env?.MONGODB_URI;
+const DB_NAME = process.env?.DB_NAME;
 
 const corsConfig = {
     origin: CORS_ORIGIN,
@@ -21,6 +23,8 @@ export {
     NODE_ENV,
     PORT,
     CORS_ORIGIN,
+    MONGODB_URI,
+    DB_NAME,
     corsConfig,
     expressJsonConfig,
     expressStaticConfig,
