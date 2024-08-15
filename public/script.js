b0efb2d4a2ef512dbf6dd99af9625b17d8485d1a
Snigdha Mistry <150170898+snigdhamistry@users.noreply.github.com>
2024-08-15 14:58:43 +0530

    adding project section and move script tag from html file to script file

---
+++ public/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed('#element', {
+        strings: ['Krishnendu Das .', 'Sneha Karmakar .', 'Anirban Das .', 'Snigdha Mistry .'],
+        typeSpeed: 50,
+    });
+});
