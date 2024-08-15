7c625911897fba1a854415609c2c330c514b6831
Snigdha Mistry <150170898+snigdhamistry@users.noreply.github.com>
2024-08-15 15:50:38 +0530

    formating files

--- public/script.js
+++ public/script.js
@@ -1,6 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var typed = new Typed('#element', {
-        strings: ['Krishnendu Das .', 'Sneha Karmakar .', 'Anirban Das .', 'Snigdha Mistry .'],
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed("#element", {
+        strings: [
+            "Krishnendu Das .",
+            "Sneha Karmakar .",
+            "Anirban Das .",
+            "Snigdha Mistry .",
+        ],
         typeSpeed: 50,
     });
 });
